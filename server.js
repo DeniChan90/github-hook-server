d582ae422517e367dd67a330df7cf258179e90d5
Deni Chan <dchan@Denis-MacBook-Pro.local>
2019-10-27 15:35:48 +0200

    test express-github-webhook

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const express = require('express');
+const GithubWebHook = require('express-github-webhook');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = process.env.PORT || 8000;
+const webhookHandler = GithubWebHook({ path: '/', secret: 'qwerty123' });
+
+app.use(bodyParser.json());
+app.use(webhookHandler);
+
+webhookHandler.on('*', (event, repo, data) => {
+  console.log('event ->>>> ', event);
+  console.log('repo ->>>>>> ', repo);
+  console.log('data ->>>>>> ', data);
+
+});
 
 app.listen(port, (err) => {
   if (err) {
