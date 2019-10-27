3b0fabe6fb4ecdd78d5fb9ef269023dfa0d8eab0
Deni Chan <dchan@Denis-MacBook-Pro.local>
2019-10-27 16:27:11 +0200

    added shell

--- server.js
+++ server.js
@@ -1,22 +1,58 @@
+const fs =  require('fs');
 const express = require('express');
+const axios = require('axios');
 const GithubWebHook = require('express-github-webhook');
 const bodyParser = require('body-parser');
+const shell = require('shelljs');
+const { App } = require("@octokit/app");
+// const Octokit = require('@octokit/rest');
 
-const app = express();
+// const APP_ID = '44821';
+// const CLIENT_ID = 'Iv1.7dedba7c70dc0d78';
+// const PRIVATE_KEY = fs.readFileSync('./webhook-app-dc.private-key.pem');
+
+const server = express();
 const port = process.env.PORT || 8000;
 const webhookHandler = GithubWebHook({ path: '/', secret: 'qwerty123' });
 
-app.use(bodyParser.json());
-app.use(webhookHandler);
+//const githubApp = new App({ id: APP_ID, privateKey: PRIVATE_KEY });
+// const jwt = githubApp.getSignedJsonWebToken();
 
-webhookHandler.on('*', (event, repo, data) => {
-  console.log('event ->>>> ', event);
-  console.log('repo ->>>>>> ', repo);
-  console.log('data ->>>>>> ', data);
+
+server.use(bodyParser.json());
+server.use(webhookHandler);
+
+webhookHandler.on('pull_request', (event, repo, data) => {
+
+  shell.exec('git clone https://github.com/DeniChan90/Github-hook');
+  shell.cd('./Github-hook');
+  shell.exec('openapi validate openapi.yaml', (code, stdout, stderr) => {
+
+    axios({
+      method: 'post',
+      url: `${data.issue_url}/comments`,
+      headers: {
+        Authorization: 'token 15d247a2311494adf0f2c55518e7d9668f57c0db',
+        // Authorization: `Bearer ${jwt}`,
+        // Accept: 'application/vnd.github.machine-man-preview+json'
+      },
+      data : {
+        body: 'test comment'
+      }
+    }).then((res) => {
+      console.log('response ... ', res);
+
+    }).catch((err) => {
+      console.log('error  ... ', err);
+    });
+
+  });
+  shell.cd('./Github-hook');
+  shell.exec('rm -rf Github-hook');
 
 });
 
-app.listen(port, (err) => {
+server.listen(port, (err) => {
   if (err) {
     return console.log('Error !!!\n', err)
   }
